557498f6b5fc4290cd7ab7dbbe66c83982ab0c03
hossam <hossam9k@gmail.com>
2024-04-16 23:32:46 -0700

    code enhancement

--- app/src/main/java/com/example/cfh/home/HomeScreen.kt
+++ app/src/main/java/com/example/cfh/home/HomeScreen.kt
@@ -1,8 +1,6 @@
 package com.example.cfh.home
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -11,13 +9,9 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.unit.dp
@@ -31,39 +25,38 @@ fun HomeScreen(
     drawerState: DrawerState,
     venueViewModel: VenueViewModel = hiltViewModel()
 ) {
-    val state = venueViewModel.uiState.collectAsState()
     val venues = venueViewModel.venues.collectAsState(initial = emptyList()).value
 
     Scaffold(
         topBar = { AppBar(drawerState = drawerState) }
     ) {
         LazyColumn(modifier = Modifier.padding(it)) {
-            item {
-                val checked = remember { mutableStateOf(true) }
-                Column(
-                    modifier = Modifier
-                        .padding(25.dp)
-                        .fillMaxSize(),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-
-                    Switch(
-                        checked = checked.value,
-                        onCheckedChange = { checked.value = it }
-                    )
-                    // Image(painter = if (checked.value) painterResource(id = R.drawable.img_bulb_on) else  painterResource(id = R.drawable.img_bulb_off), contentDescription = "Bulb Status")
-
-                    if (checked.value) {
-                        Text("Map is on")
-                    } else {
-                        Text("Map is off")
-                    }
-                }
-            }
-            items(venues) { post ->
+//            item {
+//                val checked = remember { mutableStateOf(true) }
+//                Column(
+//                    modifier = Modifier
+//                        .padding(25.dp)
+//                        .fillMaxSize(),
+//                    verticalArrangement = Arrangement.Center,
+//                    horizontalAlignment = Alignment.CenterHorizontally,
+//                ) {
+//
+//                    Switch(
+//                        checked = checked.value,
+//                        onCheckedChange = { checked.value = it }
+//                    )
+//                    // Image(painter = if (checked.value) painterResource(id = R.drawable.img_bulb_on) else  painterResource(id = R.drawable.img_bulb_off), contentDescription = "Bulb Status")
+//
+//                    if (checked.value) {
+//                        Text("Map is on")
+//                    } else {
+//                        Text("Map is off")
+//                    }
+//                }
+//            }
+            items(venues) { venue ->
                 // For each post in the list, display a PostCard
-                PostCard(post)
+                PostCard(venue)
             }
         }
     }
@@ -71,7 +64,7 @@ fun HomeScreen(
 
 
 @Composable
-fun PostCard(postEntity: VenueEntity?) {
+fun PostCard(venueEntity: VenueEntity?) {
     Card(
         modifier = Modifier
             .padding(8.dp)
@@ -83,15 +76,15 @@ fun PostCard(postEntity: VenueEntity?) {
                 .padding(16.dp)
         ) {
             Text(
-                text = "User ID: ${postEntity?.name}",
+                text = "${venueEntity?.name}",
                 style = MaterialTheme.typography.titleMedium
             )
             Text(
-                text = "Title: ${postEntity?.categories?.name}",
+                text = "${venueEntity?.categories?.name}",
                 style = MaterialTheme.typography.titleSmall
             )
             Text(
-                text = "Body: ${postEntity?.location?.address}",
+                text = "${venueEntity?.location?.address}",
                 style = MaterialTheme.typography.bodyMedium
             )
         }
